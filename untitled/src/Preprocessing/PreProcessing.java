0ef2f50d959266baac51810b94e1f0cc7715e467
NoahMeissner <108337767+NoahMeissner@users.noreply.github.com>
2023-06-27 10:57:37 +0200

    test

--- untitled/src/Preprocessing/PreProcessing.java
+++ untitled/src/Preprocessing/PreProcessing.java
@@ -2,10 +2,28 @@ package Preprocessing;
 
 import lingologs.Script;
 
+import java.util.List;
+
 public class PreProcessing {
 
-    public PreProcessing(Script text){
+    List<Script> L;
 
+    public PreProcessing(Script text){
+        this.L = cutText(text);
+    }
+
+    /*
+        Goal is to split text into to parts to start multiprocessing part
+     */
+    private List<Script> cutText(Script S){
+        int length = S.length();
+        int mid = length /2;
+
+        Script firstHalf = S.part(0,mid);
+        Script secondHalf = S.part(mid,length);
+        System.out.println(firstHalf);
+        System.out.println(secondHalf);
+        return List.of(firstHalf, secondHalf);
     }
 
 
