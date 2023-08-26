1b35e69c595fcee6459fb2e0c256d605a438ea3d
NoahMeissner <108337767+NoahMeissner@users.noreply.github.com>
2023-08-26 15:14:15 +0200

    Delete Unecessary

--- untitled/src/Familie.java
+++
@@ -1,7 +0,0 @@
-public class Familie extends Mensch{
-    String Ledig;
-    public Familie(String Alter, String Augenfarbe, String Ledig) {
-        super(Alter, Augenfarbe);
-        this.Ledig = Ledig;
-    }
-}
